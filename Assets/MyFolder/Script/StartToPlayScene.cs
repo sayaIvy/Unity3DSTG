1888e8c789509ebc8ca52ccf2d680d20148b6e7c
sayaIvy <saya6414h@gmail.com>
2023-10-21 17:00:26 +0900

    [add]スタートシーンを追加 [fix]リザルトシーンからスタートシーンにループできるように修正

---
+++ Assets/MyFolder/Script/StartToPlayScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartToPlayScene : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Fire-A"))
+        {
+            PlayBottunClick();
+        }
+    }
+
+    public void PlayBottunClick()
+    {
+        SceneManager.LoadScene("Play");
+    }
+}
